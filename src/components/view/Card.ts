26cc275363b1b7328166b2718e3af97b4571a088
Daniel K <89999804331@yandex.ru>
2025-05-12 13:58:32 +0300

    feat: fix card.ts

--- src/components/view/Card.ts
+++ src/components/view/Card.ts
@@ -2,10 +2,11 @@ import { Component } from "../base/Component";
 import { ensureElement, formatNumber } from "../../utils/utils";
 import { IProductItem } from "../../types";
 
+// Инициализация интерфейса.
 interface ICardActions {
     onClick: (event: MouseEvent) => void;
 }
-
+// Создание класса элемент карточки.
 export class Card extends Component<IProductItem> {
     protected _title: HTMLElement;
     protected _image: HTMLImageElement;
@@ -18,8 +19,8 @@ export class Card extends Component<IProductItem> {
     constructor(protected blockName: string, container: HTMLElement, actions?: ICardActions) {
         super(container);
 
-        this._title = ensureElement<HTMLElement>(`.${blockName}__title`, container);
-        this._image = ensureElement<HTMLImageElement>(`.${blockName}__image`, container);
+        this._title = container.querySelector(`.${blockName}__title`);
+        this._image = container.querySelector(`.${blockName}__image`);
         this._button = container.querySelector(`.${blockName}__button`);
         this._category = container.querySelector(`.${blockName}__category`);
         this._description = container.querySelector(`.${blockName}__description`);
@@ -30,12 +31,6 @@ export class Card extends Component<IProductItem> {
             } else {
                 container.addEventListener('click', actions.onClick);
             }
-        }
-    }
-
-    private toggleButtonState(inBasket?: boolean) {
-        if (this._button && inBasket !== undefined) {
-            this._button.textContent = inBasket ? 'Убрать' : 'Купить';
         }
     }
 
@@ -55,18 +50,20 @@ export class Card extends Component<IProductItem> {
         this.setText(this._title, data.title);
         this.setImage(this._image, data.image, data.title);
         this.setText(this._category, data.category);
+
         if (data.price !== null && data.price !== undefined) {
             this.setText(this._price, `${formatNumber(data.price)} синапсов`);
         } else {
             this.setText(this._price, 'Бесценно');
-        }        
+        }  
+
         if (this._description) {
-            this.setText(this._description, data.description || '');
+            this.setText(this._description, data.description);
             this.setVisible(this._description); 
         }
 
         if (this._button) {
-            this.setText(this._button, data.buttonText || '')
+            this.setText(this._button, data.buttonText)
         }
         
         return this.container;
